2fcec0b76dfdeeac4d30cc147ffc2f6a6934a937
przepiorar <przepiorar@wp.pl>
2017-04-12 17:37:58 +0200

    Update Program.cs

--- GrafDwudzielny/Program.cs
+++ GrafDwudzielny/Program.cs
@@ -14,6 +14,7 @@ namespace GrafDwudzielny
         [STAThread]
         static void Main()
         {
+            //hello xD;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
