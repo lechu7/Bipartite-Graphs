ff2aa99195bfe74ec933952e3b6431d6979c00fa
lechu7 <lechu0@vp.pl>
2017-05-05 15:14:54 +0200

    Uporzadkowanie projektu

--- GrafDwudzielny/Form1.cs
+++ GrafDwudzielny/Form1.cs
@@ -12,14 +12,24 @@ namespace GrafDwudzielny
 {
     public partial class Form1 : Form
     {
+        public static Form1 INSTANCE;
         public Form1()
         {
             InitializeComponent();
+       
         }
 
+        public static Form1 getInstance()
+        {
+            if (INSTANCE == null)
+                INSTANCE = new Form1();
+
+            return INSTANCE;
+        }
         private void button1_Click(object sender, EventArgs e)
         {
-          bool wynik= GrafDwudzielny.Program.sprawdz();
+            GeneracjaGrafu.generateGraph();
+            bool wynik= Silnik.sprawdz();
           MessageBox.Show(wynik.ToString());
         }
     }
---
+++ GrafDwudzielny/GeneracjaGrafu.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GraphVizWrapper;
+using GraphVizWrapper.Commands;
+using GraphVizWrapper.Queries;
+using System.IO;
+using System.Drawing;
+
+namespace GrafDwudzielny
+{
+    class GeneracjaGrafu
+    {
+        public static Form1 glowna = GrafDwudzielny.Form1.getInstance();
+        public static byte[] generateGraph()
+        {
+
+
+            // These three instances can be injected via the IGetStartProcessQuery, 
+            //                                               IGetProcessStartInfoQuery and 
+            //                                               IRegisterLayoutPluginCommand interfaces
+
+            var getStartProcessQuery = new GetStartProcessQuery();
+            var getProcessStartInfoQuery = new GetProcessStartInfoQuery();
+            var registerLayoutPluginCommand = new RegisterLayoutPluginCommand(getProcessStartInfoQuery, getStartProcessQuery);
+
+            // GraphGeneration can be injected via the IGraphGeneration interface
+
+            var wrapper = new GraphGeneration(getStartProcessQuery,
+                                              getProcessStartInfoQuery,
+                                              registerLayoutPluginCommand);
+           
+           
+            byte[] output = wrapper.GenerateGraph("digraph{a -> b; b -> c; c -> a;}", Enums.GraphReturnType.Png);
+            return output;
+           
+        }
+    }
+}
