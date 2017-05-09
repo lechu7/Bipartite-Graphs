c03c9028283c055f0b51a5b5b999c830f152a054
lechu7 <lechu0@vp.pl>
2017-05-09 22:34:58 +0200

    Fix ico

--- GrafDwudzielny/Form1.Designer.cs
+++ GrafDwudzielny/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
@@ -179,6 +180,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(823, 665);
             this.MinimumSize = new System.Drawing.Size(823, 665);
